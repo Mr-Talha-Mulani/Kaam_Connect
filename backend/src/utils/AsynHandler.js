18c10b517d5c214017e2f7a48de48dabfb70f7e2
Mohd Adil <shaikhmdadil191@gmail.com>
2026-03-31 16:52:09 +0530

    initialization

---
+++ backend/src/utils/AsynHandler.js
@@ -0,0 +1,14 @@
+
+
+const AsynHandler = (fn) => { 
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      console.log(err)
+      next(err);
+    }
+  };
+};
+
+export default AsynHandler; 
